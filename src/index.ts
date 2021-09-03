3f40401acb2057c7c7abf9a9c296625f7e1f8962
Parag Jadhav <>
2021-09-03 18:10:30 +0530

    updated app entry point

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
-console.log("Hello world!");
+import Logic from "../src/lib/Logic";
+
+const message = "hello^%$*&(world";
+const stringOps = new Logic(message);
+
+const data = JSON.stringify(stringOps.duplicateCharCount());
+console.log(data);
