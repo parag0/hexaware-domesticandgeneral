6530729d476ebfdea16cb284263026c7d9c77cdf
Parag Jadhav <>
2021-09-03 19:06:39 +0530

    remoed JSON.stringiy statment

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,5 @@ import Logic from "../src/lib/Logic";
 const message = "hello^%$*&(world";
 const stringOps = new Logic(message);
 
-const data = JSON.stringify(stringOps.duplicateCharCount());
+const data = stringOps.duplicateCharCount();
 console.log(data);
